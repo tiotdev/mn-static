42515055ba2987e8cd3fc1e73515a9c04a55b362
tiotdev <tiotdev@tiot.de>
2020-11-30 12:58:22 +0100

    Simply static export

---
+++ mn/timeline/index.php
@@ -0,0 +1,376 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Geschichte von m&auml;usenews.de</title>
+<link rel="stylesheet" type="text/css" href="/mn/timeline/styles.css" />
+<script type="0efddc6405ac403c68ac7dae-text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+<script type="0efddc6405ac403c68ac7dae-text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js"></script>
+<script type="0efddc6405ac403c68ac7dae-text/javascript" src="/mn/timeline/script.js"></script>
+</head>
+<body>
+<div id="main">
+<div id="timelineLimiter"> 
+<div id="timelineScroll"> 
+<div class="event">
+<div class="eventHeading green">2004</div>
+<ul class="eventList">
+<li class="gerbil">
+<span class="icon" title="News"></span>
+Die ersten Rennm&auml;use
+<div class="content">
+<div class="body">Der achtj&auml;hrige Julian Peters bekommt Rennm&auml;use als Haustiere. Nachdem die ersten beiden m&auml;nnlichen Rennm&auml;use, Monscho und Flinki, beide schon nach kurzer Zeit sterben, werden die Rennm&auml;use Topolino und Minimaus gekauft. Mehr im <a href="/downloads/maeusetagebuch/Kapitel2.html" target='_parent'>2. Kapitel des M&auml;usetagebuchs</a>.</div>
+<div class="title">Die ersten Rennm&auml;use</div>
+<div class="date">September 2004</div>
+</div>
+</li></ul></div>
+<div class="event">
+<div class="eventHeading blue">2005</div>
+<ul class="eventList">
+<li class="website">
+<span class="icon" title="News"></span>
+Das M&auml;usetagebuch
+<div class="content">
+<div class="body">Julian Peters Gruindschullehrerin Frau Gro&szlig;e schl&auml;gt ihm vor, ein Tagebuch &uuml;ber seine Rennm&auml;use zu schreiben. Dieser beginnt auf der Stelle und schreibt zwei Kapitel aus dem vergangenen Jahr nach.</div>
+<div class="title">Das M&auml;usetagebuch</div>
+<div class="date">Ende Februar 2005</div>
+</div>
+</li>
+<li class="gerbil">
+<span class="icon" title="News"></span>
+Die ersten Rennmausbabys
+<div class="content">
+<div class="body">Minimaus entpuppt sich als Weibchen und bringt vier kleine Rennmausbabys zur Welt. Mehr dazu im <a href="/downloads/maeusetagebuch/Kapitel3.html" target='_parent'>3. Kapitel des M&auml;usetagebuchs</a>.</div>
+<div class="title">Die ersten Rennmausbabys</div>
+<div class="date">M&auml;rz 2005</div>
+</div>
+</li>
+<li class="gerbil">
+<span class="icon" title="News"></span>
+Topolinos Operation
+<div class="content">
+<div class="body">Damit Topolino und Minimaus sich nicht unkontrolliert vermehren, aber trotzdem zusammen bleiben k&ouml;nnen, wird Topolino kastriert. Mehr dazu im <a href="/downloads/maeusetagebuch/Kapitel4.html" target='_parent'>4. Kapitel des M&auml;usetagebuchs</a>.</div>
+<div class="title">Topolinos Operation</div>
+<div class="date">M&auml;rz/ April 2005</div>
+</div>
+</li>
+<li class="gerbil">
+<span class="icon" title="News"></span>
+Die zweiten Rennmausbabys
+<div class="content">
+<div class="body">Topolino und Minimaus bekommen noch einmal vier Rennmausbabys. Mehr dazu im <a href="/downloads/maeusetagebuch/Kapitel5.html" target='_parent'>5. Kapitel des M&auml;usetagebuchs</a>.</div>
+<div class="title">Die zweiten Rennmausbabys</div>
+<div class="date">Ende April 2005</div>
+</div>
+</li>
+<li class="gerbil">
+<span class="icon" title="News"></span>
+Honeybee
+<div class="content">
+<div class="body">Ein Rennmausbaby darf bleiben und wird auf den Namen Honeybee getauft. Mehr dazu im <a href="/downloads/maeusetagebuch/Kapitel7.html" target='_parent'>7. Kapitel des M&auml;usetagebuchs</a></div>
+<div class="title">Honeybee</div>
+<div class="date">Juni 2005</div>
+</div>
+</li>
+</ul></div>
+<div class="event">
+<div class="eventHeading green">2006</div>
+<ul class="eventList">
+<li class="gerbil">
+<span class="icon" title="Milestone"></span>
+Das M&auml;uselabyrinth
+<div class="content">
+<div class="body">Das M&auml;uselabyrinth wird gebaut. Mehr dazu im <a href="/downloads/maeusetagebuch/Kapitel9.html" target='_parent'>9. Kapitel des M&auml;usetagebuchs</a>.</div>
+<div class="title">Das M&auml;uselabyrinth</div>
+<div class="date">Januar/ Februar 2006</div>
+</div>
+</li><li class="gerbil">
+<span class="icon" title="News"></span>
+Ein neues Terrarium
+<div class="content">
+<div class="body">Zusatzlich zu ihrem K&auml;fig bekommen die Rennm&auml;use nun ein ausgediehntes Aquarium angeschlossen. </div>
+<div class="title">Ein neues Terrarium</div>
+<div class="date">Mai 2006</div>
+</div>
+</li></ul></div>
+<div class="event">
+<div class="eventHeading blue">2007</div>
+<ul class="eventList">
+<li class="gerbil">
+<span class="icon" title="Milestone"></span>
+Honeybee stirbt
+<div class="content">
+<div class="body">Das Leben der Rennmaus Honeybee ist zu Ende.</div>
+<div class="title">Honeybee stirbt</div>
+<div class="date">Ende 2007</div>
+</div>
+</li></ul></div>
+<div class="event">
+<div class="eventHeading chreme">2008</div>
+<ul class="eventList">
+<li class="website">
+<span class="icon" title="Milestone"></span>
+Rennmaus-Referat & 1. M&auml;usenewsletter
+<div class="content">
+<div class="body">Die F&uuml;nftkl&auml;ssler Jonas Knapp un Julian Peters halten zusammen ein Referat &uuml;ber Rennm&auml;use. Da viele aus der Klasse das M&auml;usetagebuch lesen m&ouml;chten, k&ouml;nnen sich diese in eine E-Mail-Liste eintragen und sollen nun alle zwei Wochen ein Kapitel des M&auml;usetagebuchs zugeschickt bekommen. Schon am Tag des Referats erscheint der erste M&auml;usenewsletter - noch als E-Mail-Text.</div>
+<div class="title">Rennmaus-Referat & 1. M&auml;usenewsletter</div>
+<div class="date">11. April 2008</div>
+</div>
+</li>
+<li class="website">
+<span class="icon" title="News"></span>
+Der zweite M&auml;usenewsletter
+<div class="content">
+<div class="body">Im zweiten M&auml;usenewsletter wird nur mitgeteilt, dass der M&auml;usenewsletter nur alle zwei Wochen erscheint.</div>
+<div class="title">Der zweite M&auml;usenewsletter</div>
+<div class="date">25. April 2008</div>
+</div>
+</li>
+<li class="website">
+<span class="icon" title="News"></span>
+Der dritte M&auml;usenewsletter
+<div class="content">
+<div class="body">Der dritte M&auml;usenewsletter erscheint im PDF-Format und beinhaltet die Fortsetzungs-Geschichte 'FC Mehlwurm', die von Lesern weitergeschrieben werden soll.</div>
+<div class="title">Der dritte M&auml;usenewsletter</div>
+<div class="date">8. Mai 2008</div>
+</div>
+</li>
+<li class="website">
+<span class="icon" title="News"></span>
+Der vierte M&auml;usenewsletter
+<div class="content">
+<div class="body">Der vierte M&auml;usenewsletter erscheint - allerdings ohne eine weitere Folge des 'FC mehlwurm', da niemand eine Fortsetzung geschrieben hat.</div>
+<div class="title">Der vierte M&auml;usenewsletter</div>
+<div class="date">23. Mai 2008</div>
+</div>
+<li class="website">
+<span class="icon" title="News"></span>
+Der f&uuml;nfte M&auml;usenewsletter
+<div class="content">
+<div class="body">Zum f&uuml;nften M&auml;usenewsletter kommt Darius M. mit ins bisher nur aus Julian Peters bestehende Newsletter-Team. Der M&auml;usenewsletter beinhaltet tolle Aktionen zur Fu&szlig;ball-WM mit sensationellen Preisen (Fu&szlig;ball, Buch,...). Nachdem keine Reaktionen auf den Newsletter kommen, wird dieser vorerst eingestellt.</div>
+<div class="title">Der f&uuml;nfte M&auml;usenewsletter</div>
+<div class="date">6. Juni 2008</div>
+</div>
+</li>
+<li class="gerbil">
+<span class="icon" title="News"></span>
+Minimaus stirbt
+<div class="content">
+ <div class="body">Minimaus stirbt im Rennmausalter von &uuml;ber drei Jahren.</div>
+<div class="title">Minimaus stirbt</div>
+<div class="date">Ende 2008</div>
+</div>
+</ul></div>
+<div class="event">
+<div class="eventHeading green">2009</div>
+<ul class="eventList">
+<li class="gerbil">
+<span class="icon" title="News"></span>
+Topolino stirbt
+<div class="content">
+<div class="body">Topolino stirbt im hohen Alter von mehr als dreieinhalb Jahren. Vorerst werden kiene neuen Rennm&auml;use angeschafft.</div>
+<div class="title">Topolino stirbt.</div>
+<div class="date">Februar 2009</div>
+</div>
+<li class="website">
+<span class="icon" title="News"></span>
+HTML
+<div class="content">
+<div class="body">Julian Peters beginnt mit der Hilfe seines Lateinlehrers Herr Liebmann, sich mit HTML zu besch&auml;ftigen, um eine Websseite f&uuml;r den M&auml;usenewsletter zu erstellen. Mit gr&ouml;&szlig;erem Publikum als nur 10 Leuten erhofft er sich eine Fortf&uuml;hrung des M&auml;usenewsletters - vorerst ohne Rennm&auml;use. Seine HTML-Kenntnisse machen immer gr&auml;&szlig;ere Fortschitte, so dass im September ein Grundger&uuml;st der Webseite steht.</div>
+<div class="title">HTML</div>
+<div class="date">M&auml;rz 2009</div>
+</div>
+</li>
+<li class="gerbil">
+<span class="icon" title="News"></span>
+Die Rennm&auml;use
+<div class="content">
+<div class="body">Julian Peters bekommt seine dritten Rennm&auml;use, vier an der Zahl.</div>
+<div class="title">Die Rennm&auml;use</div>
+<div class="date">18. November 2009</div>
+</div>
+</li>
+<li class="website">
+<span class="icon" title="News"></span>
+Hosting
+<div class="content">
+<div class="body">Beim Onlinestellen der Website gibt es immense Probleme. Schlie&szlig;lich kommt Julian Peters nicht darum, sich zu Weihnachten ein Jahr kostenpflichtiges Webhosting zu w&uuml;nschen - welches er auch geschenkt bekommt.</div>
+<div class="title">Hosting</div>
+<div class="date">Dezember 2009</div>
+</div>
+</li>
+<li class="website">
+<span class="icon" title="News"></span>
+Die Webseite geht online!
+<div class="content">
+<div class="body">Am 28. Dezember 2009 geht die Webseite online, vorerst noch im Testmodus. Es dauert einige Tage, bis alle Fehler behoben sind.</div>
+<div class="title">Die Webseite geht online!</div>
+<div class="date">28. Dezember 2009</div>
+</div>
+</li></ul></div>
+<div class="event">
+<div class="eventHeading blue">2010</div>
+<ul class="eventList">
+<li class="website">
+<span class="icon" title="News"></span>
+Der 6. M&auml;usenewsletter
+<div class="content">
+<div class="body">Das M&auml;usenewsletter-team bringt den 6. M&auml;usenewsletter heraus. Dadurch bekommt die Webseite erstmals ein paar Besucher.</div>
+<div class="title">Der 6. M&auml;usenewsletter</div>
+<div class="date">1. Januar 2010</div>
+</div>
+</li>
+<li class="gerbil">
+<span class="icon" title="News"></span>
+Namen f&uuml;r die Rennm&auml;use
+<div class="content">
+<div class="body">Nun bekommen die vier Rennm&auml;use endlich Namen! Sie hei&szlig;en jetzt Mr. Spot, Mr. Speedy, Mr. Blue und Mr. Red. Mehr dazu findest du in <a href="/maeusenews_aktuell/index2.html#Die Namensgebung" target='_parent'>m&auml;usenews aktuell</a></div>
+<div class="title">Namen f&uuml;r die Rennm&auml;use</div>
+<div class="date">6. Januar 2010</div>
+</div>
+</li>
+<li class="website">
+<span class="icon" title="News"></span>
+Neues Layout der Webseite
+<div class="content">
+<div class="body">Die Webseite erh&auml;lt ein komplett neues Layout - welches leider nur im Browser Firefox ordnungsgem&auml;&szlig; funktioniert.</div>
+<div class="title">Neues Layout der Webseite</div>
+<div class="date">Anfang M&auml;rz 2010</div>
+</div>
+</li>
+<li class="website">
+<span class="icon" title="News"></span>
+Der 7. M&auml;usenewsletter
+<div class="content">
+<div class="body">Der 7. M&auml;usenewsletter erscheint - mit einem Aprilscherz und einem gro&szlig;en Oster-Spezial. Darius M., dem die Arbeit zu viel wird, tritt wenig sp&auml;ter aus dem M&auml;usenewsletter-Team, welches Julian Peters trotzdem unter diesem Namen fortf&uuml;hrt, aus.</div>
+<div class="title">Der 7. M&auml;usenewsletter</div>
+<div class="date">1. April 2010</div>
+</div>
+</li>
+<li class="website">
+<span class="icon" title="News"></span>
+Der 8. M&auml;usenewsletter
+<div class="content">
+<div class="body">Der 8. M&auml;usenewsletter erscheint um einen Monat vorgezogen - wegen der Fu&szlig;ball-WM, &uuml;ber die es ein gro&szlig;es WM-Spezial und einen tollen <a href="/downloads/wmplaner/" target='_parent'>WM-Planer</a> gibt.</div>
+<div class="title">Der 8. M&auml;usenewsletter</div>
+<div class="date">1. Juni 2010</div>
+</div>
+</li>
+<li class="website">
+<span class="icon" title="News"></span>
+Der 9. M&auml;usenewsletter
+ <div class="content">
+<div class="body">Noch ein Spezial-Newsletter? - Ja! Der 9. M&auml;usenewsletter kommt mit einem gro&szlig;em Halloween-Spezial. Au&szlig;erdem erscheint das Spiel <a href="/sonstiges/spiele/die-abenteuer-der-wuestenrennmaus/" target='_parent'>'Die Abenteuer der W&uuml;stenrennmaus'</a> von Arnold Weber, welcher dem M&auml;usenewsletter-Team beitritt. Das Spiel erreicht eine sehr hohe Downloadhzahl.</div>
+<div class="title">Der 9. M&auml;usenewsletter</div>
+<div class="date">1. Oktober 2010</div>
+</div>
+<li class="website">
+<span class="icon" title="News"></span>
+m&auml;usenews.de Adventskalender
+<div class="content">
+<div class="body">Der m&auml;usenews.de Adventskalender 2010 wird ein voller Erfolg; teilweise steigen die Besucherzahlen auf &uuml;ber 100 an einem Tag an. Der Comiczeichner Vincent zeichnet f&uuml;r m&auml;usenews.de f&uuml;r jeden Tag eine M&auml;usebild und wird ins M&auml;usenewsletter-Team aufgenommen.
+</div>
+<div class="title">m&auml;usenews.de Adventskalender 2010</div>
+<div class="date">Dezember 2010</div>
+</div>
+</li>
+</li>
+</ul></div>
+<div class="event">
+<div class="eventHeading chreme">2011</div>
+<ul class="eventList">
+<li class="website">
+<span class="icon" title="News"></span>
+Der 10. M&auml;usenewsletter
+<div class="content">
+<div class="body">Der zehnte M&auml;usenewsletter erscheint leider mit zwei Monaten und einem Tag versp&auml;tung - daf&uuml;r ist er allerdings auch der umfangreichste bisher: Die Webseite bekomt ein neues Layout und viele neue Funktionen! Der dem M&auml;usenewsletter-Team beigetretene Flash-Programmierer Jonas Knapp hat daf&uuml;r das Spiel Mausotopia, eine Slideshow und ein Banner programmiert.</div>
+<div class="title">Der 10. M&auml;usenewsletter</div>
+<div class="date">7. M&auml;rz 2011</div>
+</div>
+</li>
+<li class="website">
+<span class="icon" title="News"></span>
+Der 11. M&auml;usenewsletter
+<div class="content">
+<div class="body">Der elfte M&auml;usenewsletter erscheint am 1. April 2011 und bringt das Spiel <a href="http://xn--musenews-za.de/sonstiges/spiele/die-abenteuer-der-wuestenrennmaus">"Die Abenteuer der W&uuml;stenrennmaus 2"</a> mit sich. Au&ssml;erdem erscheint eine Bauaneitung f&uuml;r ein Rennmaus-Labyrinth.</div>
+<div class="title">Der 11. M&auml;usenewsletter</div>
+<div class="date">1. April 2011</div>
+</div> </li>
+<li class="website">
+<span class="icon" title="News"></span>
+Der Newsletter wird ein richtiger Newsletter
+<div class="content">
+<div class="body">Der Mäusenewsletter ist nun ein "richtiger" Newsletter, der nicht mehr als PDF sondern als Email kommt.</div>
+ <div class="title">Ein richtiger Newsletter</div>
+<div class="date">1. Juli 2011</div>
+</div>
+</li>
+<li class="gerbil">
+<span class="icon" title="News"></span>
+Streit
+<div class="content">
+<div class="body">Es kommt zum blutigen Streit zwischen Mr. Spot und Mr. Speedy, deshalb wird die Vierergruppe (man sollte eh keine Vierergruppen halten) in die zwei Zweiergruppen Spot-Red und Speedy-Blue aufgeteilt-</div>
+<div class="title">Streit</div>
+<div class="date">12. Juli 2011</div>
+</div>
+</li>
+<li class="gerbil">
+<span class="icon" title="News"></span>
+Mr. Speedy ist tod
+<div class="content">
+<div class="body">Mr. Speedy stirbt unerwartet. Eine Vergesellschaftung von Mr. Blue schlägt fehl.</div>
+<div class="title">Mr. Speedy ist tod</div>
+<div class="date">6. Oktober 2011</div>
+</div>
+</li>
+</ul></div>
+<div class="event">
+<div class="eventHeading orange">2012</div>
+<ul class="eventList">
+<li class="website">
+<span class="icon" title="News"></span>
+Der Mäusenewsletter erscheint monatlich
+<div class="content">
+<div class="body">Ab sofort erscheint der Mäusenewsletter immer am 1. Sonntag des Monats.</div>
+<div class="title">Der Newsletter erscheint monatlich</div>
+<div class="date">8. Januar 2012</div>
+</div>
+</li>
+<li class="gerbil">
+<span class="icon" title="News"></span>
+Mr. Spot ist tod
+<div class="content">
+<div class="body">Mr. Spot stribt an den Folgen seiner Tumorerkrankung</div>
+<div class="title">Mr. Spotist tod</div>
+<div class="date">17. März 2012</div>
+</div>
+</li>
+<li class="website">
+<span class="icon" title="News"></span>
+MN5 - ein neues Design für mäusenews
+<div class="content">
+<div class="body">Unter dem Codenamen "MN5" bekommt die Webseite ein in den Monaten davor von Usern getestetes neues Design, das nun wesentlich professioneller wirkt.</div>
+<div class="title">MN5 - ein neues Design für mäusenews</div>
+<div class="date">April 2012</div>
+</div>
+</li>
+</ul></div>
+<div class="clear"></div>
+</div>
+<div id="scroll"> 
+<div id="centered"> 
+<div id="highlight"></div> 
+<div class="scrollPoints">2004</div><div class="scrollPoints">2005</div><div class="scrollPoints">2006</div><div class="scrollPoints">2007</div><div class="scrollPoints">2008</div><div class="scrollPoints">2009</div><div class="scrollPoints">2010</div><div class="scrollPoints">2011</div><div class="scrollPoints">2012</div> 
+<div class="clear"></div>
+</div>
+</div>
+<div id="slider"> 
+<div id="bar"> 
+<div id="barLeft"></div> 
+<div id="barRight"></div> 
+</div>
+</div>
+</div>
+</div>
+<script src="https://ajax.cloudflare.com/cdn-cgi/scripts/7089c43e/cloudflare-static/rocket-loader.min.js" data-cf-settings="0efddc6405ac403c68ac7dae-|49" defer=""></script></body>
+</html>
